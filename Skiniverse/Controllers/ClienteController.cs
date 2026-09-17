9cf5071268f5290809aeb6a7d3b30aa00c0df039
ADSO <geritaal12@gmail.com>
2026-09-17 08:00:58 -0500

    Hola

---
+++ Skiniverse/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skiniverse.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
